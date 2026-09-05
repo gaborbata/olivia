7cb7bc3cd8bf8ec02cfb673da4060dd792c44f09
Gabor Bata <bata.gabor@gmail.com>
2026-09-05 14:27:58 +0200

    feat: update 2026-09-05

--- convvideo540.sh
+++ convvideo540.sh
@@ -10,6 +10,6 @@ echo Convert video to webm...
 echo convvideo.sh input ss to scale crf
 echo convvideo.sh $input $ss $to $scale $crf
 rm ffmpeg2pass*.log
-ffmpeg -hide_banner -i "$input" -pass 1 -vcodec libvpx-vp9 -deadline good -cpu-used 0 -pix_fmt yuv420p -b:v 640k -minrate 320k -maxrate 928k -crf $crf -vf scale=$scale:-2 -ac 1 -acodec libopus -b:a 32k -sn -ss $ss -to $to "$input.sd.webm" && \
-ffmpeg -hide_banner -i "$input" -pass 2 -y -vcodec libvpx-vp9 -deadline good -cpu-used 0 -pix_fmt yuv420p -b:v 640k -minrate 320k -maxrate 928k -crf $crf -vf scale=$scale:-2 -ac 1 -acodec libopus -b:a 32k -sn -ss $ss -to $to "$input.sd.webm" && \
+ffmpeg -hide_banner -i "$input" -pass 1 -vcodec libvpx-vp9 -deadline good -cpu-used 1 -pix_fmt yuv420p -b:v 640k -minrate 320k -maxrate 928k -crf $crf -vf scale=$scale:-2 -ac 1 -acodec libopus -b:a 32k -sn -ss $ss -to $to "$input.sd.webm" && \
+ffmpeg -hide_banner -i "$input" -pass 2 -y -vcodec libvpx-vp9 -deadline good -cpu-used 1 -pix_fmt yuv420p -b:v 640k -minrate 320k -maxrate 928k -crf $crf -vf scale=$scale:-2 -ac 1 -acodec libopus -b:a 32k -sn -ss $ss -to $to "$input.sd.webm" && \
 rm ffmpeg2pass*.log
